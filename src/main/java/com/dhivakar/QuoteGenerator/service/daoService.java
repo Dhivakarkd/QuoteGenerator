1a71728edd1cc20963c1fd4d7cb9169b87900a4e
Dhivakarkd <dhivakar084.github.com>
2021-04-18 17:22:24 +0530

    Code Cleanup

--- src/main/java/com/dhivakar/QuoteGenerator/service/daoService.java
+++ src/main/java/com/dhivakar/QuoteGenerator/service/daoService.java
@@ -3,16 +3,14 @@ package com.dhivakar.QuoteGenerator.service;
 import com.dhivakar.QuoteGenerator.model.Quote;
 import com.dhivakar.QuoteGenerator.model.QuoteUI;
 import com.dhivakar.QuoteGenerator.repository.QuoteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+
 public class daoService {
 
-    @Autowired
+
     QuoteRepository repository;
 
     public List<QuoteUI> findall(){
