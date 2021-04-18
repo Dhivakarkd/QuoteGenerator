c68b306fa5d519713721c2b4825fca1a13b866a2
Dhivakarkd <dhivakar084.github.com>
2021-04-18 17:35:53 +0530

    Init

---
+++ bin/data-postgres.sql
@@ -0,0 +1,3 @@
+insert into Quote(author,quote) values ('Nelson Mandela,The greatest glory in living lies not in never falling', 'but in rising every time we fall.');
+insert into Quote(author,quote) values ('Walt Disney','The way to get started is to quit talking and begin doing.'');
+insert into Quote(author,quote) values ('John Lennon','Life is what happens when you''re busy making other plans.');
