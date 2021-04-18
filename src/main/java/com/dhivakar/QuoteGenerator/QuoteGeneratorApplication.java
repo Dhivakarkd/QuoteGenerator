669d2173ea4917ebef4291eab59f2966f3d6ed90
Dhivakarkd <dhivakar084.github.com>
2021-04-18 12:28:40 +0530

    Initial commit

---
+++ src/main/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.dhivakar.QuoteGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhivakar.QuoteGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
