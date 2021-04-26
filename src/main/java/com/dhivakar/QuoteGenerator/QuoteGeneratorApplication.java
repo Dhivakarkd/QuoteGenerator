ecb9a667e6bd5f68a3a4f92b290311c1f1c932b5
Dhivakarkd <dhivakar084.github.com>
2021-04-26 15:41:56 +0530

    app config

--- src/main/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplication.java
+++ src/main/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplication.java
@@ -2,10 +2,10 @@ package com.dhivakar.QuoteGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
-@EnableJpaRepositories
+
 public class QuoteGeneratorApplication {
 
     public static void main(String[] args) {
