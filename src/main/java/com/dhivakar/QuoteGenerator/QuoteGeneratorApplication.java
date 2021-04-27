fd762262f015984063726c3470b7d949eb80e434
Dhivakarkd <dhivakar084.github.com>
2021-04-27 19:44:35 +0530

    Repository Config

--- src/main/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplication.java
+++ src/main/java/com/dhivakar/QuoteGenerator/QuoteGeneratorApplication.java
@@ -2,11 +2,13 @@ package com.dhivakar.QuoteGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-
+@EnableJpaRepositories
 public class QuoteGeneratorApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(QuoteGeneratorApplication.class, args);
--- src/main/java/com/dhivakar/QuoteGenerator/repository/QuoteRepository.java
+++ src/main/java/com/dhivakar/QuoteGenerator/repository/QuoteRepository.java
@@ -2,8 +2,9 @@ package com.dhivakar.QuoteGenerator.repository;
 
 import com.dhivakar.QuoteGenerator.model.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Integer> {
 
 }
