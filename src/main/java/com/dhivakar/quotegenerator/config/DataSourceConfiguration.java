ef81981ac38cc40d57927eaca24a8270eedea24f
Dhivakarkd <dhivakar084.github.com>
2021-08-24 22:55:16 +0530

    Added Profiles

--- src/main/java/com/dhivakar/quotegenerator/config/DataSourceConfiguration.java
+++ src/main/java/com/dhivakar/quotegenerator/config/DataSourceConfiguration.java
@@ -3,10 +3,12 @@ package com.dhivakar.quotegenerator.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Profile(value = "prod")
 public class DataSourceConfiguration {
 
     private final String url = System.getenv("DATASOURCE_URL");
