18ad4e27c61a85ce36e7b7736d663e7a60fb62dd
Dhivakarkd <dhivakar084.github.com>
2021-08-25 18:22:40 +0530

    updated test scope

--- src/test/java/com/dhivakar/quotegenerator/QuoteGeneratorApplicationTests.java
+++ src/test/java/com/dhivakar/quotegenerator/QuoteGeneratorApplicationTests.java
@@ -2,8 +2,10 @@ package com.dhivakar.quotegenerator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile(value = "dev")
 class QuoteGeneratorApplicationTests {
 
     @Test
