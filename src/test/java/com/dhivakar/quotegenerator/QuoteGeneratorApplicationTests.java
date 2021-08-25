dae51c00b32c72f1bee9e6f7655dad9a3438c86b
Dhivakarkd <dhivakar084.github.com>
2021-08-25 18:26:50 +0530

    added security config env variables

--- src/test/java/com/dhivakar/quotegenerator/QuoteGeneratorApplicationTests.java
+++ src/test/java/com/dhivakar/quotegenerator/QuoteGeneratorApplicationTests.java
@@ -2,10 +2,8 @@ package com.dhivakar.quotegenerator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile(value = "dev")
 class QuoteGeneratorApplicationTests {
 
     @Test
