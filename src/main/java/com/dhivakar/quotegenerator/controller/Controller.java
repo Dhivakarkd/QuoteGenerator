0e67846b1b256e58a45e512c88573b7bab0b83c5
Dhivakarkd <dhivakar084.github.com>
2021-08-26 19:07:15 +0530

    Add insert methods

--- src/main/java/com/dhivakar/quotegenerator/controller/Controller.java
+++ src/main/java/com/dhivakar/quotegenerator/controller/Controller.java
@@ -5,7 +5,9 @@ import com.dhivakar.quotegenerator.service.DAOservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,31 +18,38 @@ import java.util.Random;
 @Slf4j
 public class Controller {
 
-    private static final String DEV_PROFILE ="dev";
+    private static final String DEV_PROFILE = "dev";
     private final Random random = new Random();
-
+    @Autowired
+    DAOservice service;
     @Value("${spring.profiles.active}")
     private String profile;
 
-    @Autowired
-    DAOservice service;
-
     @RequestMapping(value = "/")
-    public Quote init() {
+    public ResponseEntity<Quote> init() {
         //TODO:Enable more Exception Feature
-        log.info("Generating Quote with profile :{}",profile);
+        log.info("Generating Quote with profile :{}", profile);
         return getrandomquote();
     }
 
     @GetMapping(value = "/randomQuote",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Quote getrandomquote() {
-        
-        if(profile.equalsIgnoreCase(DEV_PROFILE)) {
-            return service.findbyid(random.nextInt(8));
-        }else{
-            return service.findbyid(random.nextInt(5000));
+    public ResponseEntity<Quote> getrandomquote() {
+        Quote quote;
+        if (profile.equalsIgnoreCase(DEV_PROFILE)) {
+
+            quote = service.findbyid(random.nextInt(8));
+            if (quote != null) {
+                return ResponseEntity.ok(quote);
+            }
+        } else {
+            quote = service.findbyid(random.nextInt(5000));
+            if (quote != null) {
+                ResponseEntity.ok(quote);
+            }
         }
+        //TODO: Handle me with appropriate error page
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Quote("Not Found","Not Found"));
     }
 
     //TODO:Add Another API Mapping to handle insert feature
--- src/main/java/com/dhivakar/quotegenerator/service/DAOservice.java
+++ src/main/java/com/dhivakar/quotegenerator/service/DAOservice.java
@@ -2,12 +2,15 @@ package com.dhivakar.quotegenerator.service;
 
 import com.dhivakar.quotegenerator.model.Quote;
 import com.dhivakar.quotegenerator.repository.QuoteRepository;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class DAOservice {
 
     @Autowired
@@ -17,6 +20,18 @@ public class DAOservice {
 
         Optional<Quote> quoteOptional = repository.findById(i);
         return quoteOptional.orElse(null);
+    }
+
+    public boolean insertQuote(@NonNull Quote quote) {
+
+        try {
+            Quote updatedQuote = repository.save(quote);
+            log.info("Quote Inserted with ID : {}", updatedQuote.getId());
+            return true;
+        } catch (Exception e) {
+            log.error("Exception during Database Operation due to {}", e.getMessage());
+            return false;
+        }
     }
 
 
