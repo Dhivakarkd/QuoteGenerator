d7648dccfb8a9176d726ed75c674d17b0df83192
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-11-16 13:34:18 +0530

    modified security configs

--- src/main/java/com/dhivakar/quotegenerator/config/SecurityConfiguration.java
+++ src/main/java/com/dhivakar/quotegenerator/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.dhivakar.quotegenerator.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,8 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
-    private final String user = System.getenv("SECURITY_USER");
-    private final String password = "{noop}" + System.getenv("SECURITY_PASSWORD");
+    @Value("${api.user}")
+    private String user;
+    @Value("${api.pass}")
+    private String password;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
