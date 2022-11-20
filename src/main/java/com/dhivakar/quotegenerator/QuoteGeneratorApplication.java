8dc99b0858988c78b2881347767fa50a4ad9ce04
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-11-20 11:57:02 +0530

    added SwaggerConfiguration and SecurityConfiguration

--- src/main/java/com/dhivakar/quotegenerator/QuoteGeneratorApplication.java
+++ src/main/java/com/dhivakar/quotegenerator/QuoteGeneratorApplication.java
@@ -3,13 +3,14 @@ package com.dhivakar.quotegenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableWebMvc
+@EnableWebSecurity
 public class QuoteGeneratorApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dhivakar/quotegenerator/config/SwaggerConfiguration.java
+++ src/main/java/com/dhivakar/quotegenerator/config/SwaggerConfiguration.java
@@ -1,41 +1,58 @@
 package com.dhivakar.quotegenerator.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
+@SecurityScheme(
+        name = "basicAuth", // can be set to anything
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic",
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfiguration {
 
+
+    private static final String SCHEME_NAME = "basicAuth";
+    private static final String SCHEME = "basic";
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dhivakar.quotegenerator.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("quote-public")
+                .pathsToMatch("/quote/randomQuote")
+                .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Quote Generator API",
-                "Quote Generator is an Rest Client built using the Spring Framework . It uses a list of motivational quotes found on the web to generate new random quotes every time it runs",
-                "v1",
-                "Terms of service",
-                new Contact("Dhivakar KD", "https://github.com/Dhivakarkd", "dhivainfo084@gmail.com"),
-                "Unlicense License", "https://github.com/Dhivakarkd/QuoteGenerator/blob/master/LICENSE", Collections.emptyList());
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("quote-admin")
+                .pathsToMatch("/quote/**")
+                .build();
     }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Quote Generator API")
+                        .description("Quote Generator is an Rest Client built using the Spring Framework . It uses a list of motivational quotes found on the web to generate new random quotes every time it runs")
+                        .version("v1")
+                        .license(new License().name("Unlicense License").url("https://github.com/Dhivakarkd/QuoteGenerator/blob/master/LICENSE")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Dhivakar KD")
+                        .url("https://github.com/Dhivakarkd"));
+    }
+
 
     @Bean
     public InternalResourceViewResolver defaultViewResolver() {
