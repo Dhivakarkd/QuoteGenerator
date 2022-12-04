0c47fe8d0617b21ce83cabb398dfa819e452fd3c
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-12-04 22:28:43 +0530

    added CSRF Protection

--- src/main/java/com/dhivakar/quotegenerator/config/SecurityConfiguration.java
+++ src/main/java/com/dhivakar/quotegenerator/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -45,9 +44,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .headers().frameOptions().disable().and()
-                .cors().disable()
+        http.headers().frameOptions().disable().and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITLIST).permitAll()
                 .anyRequest().authenticated()
